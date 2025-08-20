27b44e5b2a89dc6cca66a837586df2a092c1bf00
Celil Berkay Torun <cbtorun32@gmail.com>
2025-08-20 12:43:24 +0300

    Giris_Sayfasi_Bitmis_Hali

---
+++ LevelYorum/KayitOl.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="KayitOl.aspx.cs" Inherits="LevelYorum.KayitOl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Assets/CSS/KayitOl.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="loginbox">
+                <img src="../Foto/LevelYorum.png" alt="Alternate Text" class="loginLogo" />
+                <asp:Panel ID="pnl_basarisiz" runat="server" CssClass="basarisiz" Visible="false">
+                    <asp:Label ID="lbl_basarisiz" runat="server"></asp:Label>
+                </asp:Panel>
+                <div style="margin-bottom: 10px" class="isim">
+                    <label style="margin-right: 65px">İsim:</label>
+                    <asp:TextBox ID="tb_isim" runat="server" />
+                </div>
+                <div style="margin-bottom: 10px" class="soyIsim">
+                    <label style="margin-right: 34px">Soy İsim:</label>
+                    <asp:TextBox ID="tb_soyisim" runat="server" />
+                </div>
+                <div style="margin-bottom: 10px" class="kullaniciAdi">
+                    <label>Kullanıcı Adı:</label>
+                    <asp:TextBox ID="tb_kullaniciAdi" runat="server" />
+                </div>
+                <div class="sifre">
+                    <label style="margin-right: 62px">Şifre:</label>
+                    <asp:TextBox ID="tb_sifre" runat="server" TextMode="Password" />
+                </div>
+                <div class="girisButon">
+                    <asp:LinkButton ID="lbtn_kayit" runat="server" OnClick="lbtn_kayit_Click">Kayıt Ol</asp:LinkButton>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LevelYorum/KayitOl.aspx.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LevelYorum
+{
+    public partial class KayitOl : System.Web.UI.Page
+    {
+        DataModel dm = new DataModel();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lbtn_kayit_Click(object sender, EventArgs e)
+        {
+            if (tb_kullaniciAdi.Text != "" && tb_isim.Text != "" && tb_soyisim.Text != "" && tb_sifre.Text != "")
+            {
+                if (dm.VeriKontrol("Kullanicilar", "KullaniciAdi", tb_kullaniciAdi.Text))
+                {
+                    Kullanicilar k = new Kullanicilar();
+                    k.Isim = tb_isim.Text;
+                    k.Soyisim = tb_soyisim.Text;
+                    k.KullaniciAdi = tb_kullaniciAdi.Text;
+                    k.Sifre = tb_sifre.Text;
+                    k.Durum = true;
+                    k.KullaniciTurID = 3;
+                    if (dm.KullaniciEkle(k))
+                    {
+                        Session["kullanici"] = k;
+                        Response.Redirect("Default.aspx");
+                    }
+                    else
+                    {
+                        pnl_basarisiz.Visible = true;
+                        lbl_basarisiz.Text = "Kayıt İşlemi Gerçekleşmedi";
+                    }
+                }
+                else
+                {
+                    pnl_basarisiz.Visible = true;
+                    lbl_basarisiz.Text = "Kullanıcı Adı Daha Önce Alınmıştır";
+                }
+            }
+            else
+            {
+                pnl_basarisiz.Visible = true;
+                lbl_basarisiz.Text = "Lütfen Kullanıcı Adını Veya İsmini Yazınız";
+            }
+        }
+    }
+}
